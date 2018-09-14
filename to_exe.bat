9f2fa8975dbe2e52a6a3babb656ebeca46f0aec0
sedthh <sedthh@gmail.com>
2018-09-14 21:05:27 +0200

    moved vlc files to dedicated folder
    - moved vlc dlls and plugins.zip to vlc folder
    - updated to_exe.bat

--- to_exe.bat
+++ to_exe.bat
@@ -1,8 +1,9 @@
 echo off
 cls
-echo A plugins.zip fajlt csomagold ki plugins\ mappava konvertalas elott!
-pyinstaller.exe -w -F -i icon.ico --onedir --hidden-import=libvlccore.dll editor.pyw
-xcopy libvlc.dll "dist\editor\"
+echo A schonherz-matrix\vlc\plugins.zip fajlt csomagold ki schonherz-matrix\plugins\ mappava konvertalas elott!
+pyinstaller.exe -w -F -i icon.ico --onedir editor.pyw
+xcopy "vlc\libvlc.dll" "dist\editor\"
+xcopy "vlc\libvlccore.dll" "dist\editor\"
 xcopy /s /i "images" "dist\editor\images\"
 xcopy /s /i "plugins" "dist\editor\plugins\"
 pause
